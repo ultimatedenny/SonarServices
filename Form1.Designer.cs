22252f33f62d842b234226763b63830f1f3c37f2
sbm_deni <sbm_deni@SBMB-NB-IT0394>
2024-04-17 10:48:45 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace SonarServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.tabPage10 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox0 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl2.SuspendLayout();
+            this.tabPage9.SuspendLayout();
+            this.tabPage10.SuspendLayout();
+            this.tabPage8.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage8);
+            this.tabControl2.Controls.Add(this.tabPage9);
+            this.tabControl2.Controls.Add(this.tabPage10);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl2.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.tabControl2.Location = new System.Drawing.Point(4, 3);
+            this.tabControl2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(945, 315);
+            this.tabControl2.TabIndex = 8;
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Controls.Add(this.richTextBox1);
+            this.tabPage9.Location = new System.Drawing.Point(4, 24);
+            this.tabPage9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Size = new System.Drawing.Size(938, 284);
+            this.tabPage9.TabIndex = 3;
+            this.tabPage9.Text = "EC";
+            this.tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage10
+            // 
+            this.tabPage10.Controls.Add(this.richTextBox2);
+            this.tabPage10.Location = new System.Drawing.Point(4, 24);
+            this.tabPage10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabPage10.Name = "tabPage10";
+            this.tabPage10.Size = new System.Drawing.Size(938, 284);
+            this.tabPage10.TabIndex = 4;
+            this.tabPage10.Text = "ES";
+            this.tabPage10.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(938, 284);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Controls.Add(this.richTextBox0);
+            this.tabPage8.Location = new System.Drawing.Point(4, 24);
+            this.tabPage8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Size = new System.Drawing.Size(937, 287);
+            this.tabPage8.TabIndex = 2;
+            this.tabPage8.Text = "GENERAL";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(938, 284);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox0
+            // 
+            this.richTextBox0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox0.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox0.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox0.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox0.Name = "richTextBox0";
+            this.richTextBox0.Size = new System.Drawing.Size(937, 287);
+            this.richTextBox0.TabIndex = 8;
+            this.richTextBox0.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.tabControl1.Location = new System.Drawing.Point(4, 324);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(945, 315);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(937, 287);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "WEB";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox4.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.richTextBox4.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(937, 287);
+            this.richTextBox4.TabIndex = 9;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox3.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.richTextBox3.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(938, 285);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(938, 285);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "SONAR";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(953, 642);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(953, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 666);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "SonarQube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage9.ResumeLayout(false);
+            this.tabPage10.ResumeLayout(false);
+            this.tabPage8.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage9;
+        private System.Windows.Forms.TabPage tabPage10;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabPage tabPage8;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox0;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SonarServices
+{
+    public partial class Form1 : Form
+    {
+        private Process cmdProcess;
+        private string filePath1 = @"C:\sonarqube-9.9.0.65466\logs\ce.log";
+        private string filePath2 = @"C:\sonarqube-9.9.0.65466\logs\es.log";
+        private string filePath3 = @"C:\sonarqube-9.9.0.65466\logs\sonar.log";
+        private string filePath4 = @"C:\sonarqube-9.9.0.65466\logs\web.log";
+
+        private string backupFilePath1 = @"C:\sonarqube-9.9.0.65466\logs\ce-bak.log";
+        private string backupFilePath2 = @"C:\sonarqube-9.9.0.65466\logs\es-bak.log";
+        private string backupFilePath3 = @"C:\sonarqube-9.9.0.65466\logs\sonar-bak.log";
+        private string backupFilePath4 = @"C:\sonarqube-9.9.0.65466\logs\web-bak.log";
+
+        private Timer timer1, timer2, timer3, timer4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1 = new Timer();
+            timer1.Interval = 4000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+
+            timer2 = new Timer();
+            timer2.Interval = 4000;
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Start();
+
+            timer3 = new Timer();
+            timer3.Interval = 4000;
+            timer3.Tick += new EventHandler(timer3_Tick);
+            timer3.Start();
+
+            timer4 = new Timer();
+            timer4.Interval = 4000;
+            timer4.Tick += new EventHandler(timer4_Tick);
+            timer4.Start();
+
+
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+            cmdProcess = new Process // Initialize cmdProcess at the class level
+            {
+                StartInfo = cmdStartInfo,
+                EnableRaisingEvents = true
+            };
+            cmdProcess.OutputDataReceived += (s, args) =>
+            {
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    AppendTextToRichTextBox(args.Data + Environment.NewLine);
+                }
+            };
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+        }
+        private void SendCommandToCmd(string command)
+        {
+            try
+            {
+                if (cmdProcess != null && !cmdProcess.HasExited)
+                {
+                    cmdProcess.StandardInput.WriteLine(command);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void AppendTextToRichTextBox(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => AppendTextToRichTextBox(text)));
+            }
+            else
+            {
+                richTextBox0.AppendText(text);
+            }
+        }
+        
+
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SendCommandToCmd("cd C:\\sonarqube-9.9.0.65466\\bin\\windows-x86-64");
+            SendCommandToCmd("StartSonar.bat");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string processName = "java";
+            KillProcessByName(processName);
+
+            Application.Exit();
+        }
+
+        static void KillProcessByName(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0)
+            {
+                Console.WriteLine($"No {processName} processes found.");
+                return;
+            }
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                    Console.WriteLine($"Killed {processName} process with ID {process.Id}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error killing {processName} process: {ex.Message}");
+                }
+            }
+        }
+
+        private bool IsFileInUse(string path)
+        {
+            try
+            {
+                using (File.Open(path, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath1))
+                {
+                    if (IsFileInUse(filePath1))
+                    {
+                        File.Copy(filePath1, backupFilePath1, true);
+                        string fileContents = File.ReadAllText(backupFilePath1);
+                        richTextBox1.Text = fileContents;
+                        if (File.Exists(backupFilePath1))
+                        {
+                            File.Delete(backupFilePath1);
+                        }
+                    }
+                    else
+                    {
+                        string fileContents = File.ReadAllText(filePath1);
+                        richTextBox1.Text = fileContents;
+                    }
+                }
+                else
+                {
+                    richTextBox1.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Error reading file: " + ex.Message;
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath2))
+                {
+                    if (IsFileInUse(filePath2))
+                    {
+                        File.Copy(filePath2, backupFilePath2, true);
+                        string fileContents = File.ReadAllText(backupFilePath2);
+                        richTextBox2.Text = fileContents;
+                        if (File.Exists(backupFilePath2))
+                        {
+                            File.Delete(backupFilePath2);
+                        }
+                    }
+                    else
+                    {
+                        string fileContents = File.ReadAllText(filePath2);
+                        richTextBox2.Text = fileContents;
+                    }
+                }
+                else
+                {
+                    richTextBox2.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox2.Text = "Error reading file: " + ex.Message;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath3))
+                {
+                    if (IsFileInUse(filePath3))
+                    {
+                        File.Copy(filePath3, backupFilePath3, true);
+                        string fileContents = File.ReadAllText(backupFilePath3);
+                        richTextBox3.Text = fileContents;
+                        if (File.Exists(backupFilePath3))
+                        {
+                            File.Delete(backupFilePath3);
+                        }
+                    }
+                    else
+                    {
+                        string fileContents = File.ReadAllText(filePath3);
+                        richTextBox3.Text = fileContents;
+                    }
+                }
+                else
+                {
+                    richTextBox3.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox3.Text = "Error reading file: " + ex.Message;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath4))
+                {
+                    if (IsFileInUse(filePath4))
+                    {
+                        File.Copy(filePath4, backupFilePath4, true);
+                        string fileContents = File.ReadAllText(backupFilePath4);
+                        richTextBox4.Text = fileContents;
+                        if (File.Exists(backupFilePath4))
+                        {
+                            File.Delete(backupFilePath4);
+                        }
+                    }
+                    else
+                    {
+                        string fileContents = File.ReadAllText(filePath4);
+                        richTextBox4.Text = fileContents;
+                    }
+                }
+                else
+                {
+                    richTextBox4.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox4.Text = "Error reading file: " + ex.Message;
+            }
+        }
+    }
+}
